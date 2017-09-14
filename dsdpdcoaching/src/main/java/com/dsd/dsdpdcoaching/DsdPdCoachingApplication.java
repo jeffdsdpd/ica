b48b700869b6340bd7e9d12f694c752f471bf024
bbilyeu <bilyeu@gmail.com>
2017-09-13 20:39:15 -0400

    Adding project structure created via start.spring.io

---
+++ dsdpdcoaching/src/main/java/com/dsd/dsdpdcoaching/DsdPdCoachingApplication.java
@@ -0,0 +1,12 @@
+package com.dsd.dsdpdcoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdPdCoachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdPdCoachingApplication.class, args);
+	}
+}
---
+++ dsdpdcoaching/src/test/java/com/dsd/dsdpdcoaching/DsdPdCoachingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dsd.dsdpdcoaching;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DsdPdCoachingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
