017e8de297861c061d62cf0508abf0db30f03214
bbilyeu <bilyeu@gmail.com>
2017-09-24 11:34:10 -0400

    Enabling spring security and set up login page

---
+++ dsdpdcoaching/src/main/java/com/dsd/dsdpdcoaching/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.dsd.dsdpdcoaching.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+
+}
