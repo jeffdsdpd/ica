23070d7d33e96878ccefe8a785dd4f9db89cea31
Jeff Kish <jeff@dsdpdcoaching.com>
2022-01-27 07:21:02 -0500

    code cleanup for the ninjalearning form

---
+++ dsdpdcoaching/src/main/java/com/dsd/dsdpdcoaching/dao/NinjaLevelTrainingDao.java
@@ -0,0 +1,23 @@
+package com.dsd.dsdpdcoaching.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.dsd.dsdpdcoaching.dto.NinjaLevelTeachingData;
+
+@Repository
+@Transactional
+public class NinjaLevelTrainingDao {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	//Called by the JSONRequestController to save rubric data on rubricForm.html
+		public void saveNinjaTrainingData(NinjaLevelTeachingData data) {
+			entityManager.persist(data);		
+		}
+
+}
