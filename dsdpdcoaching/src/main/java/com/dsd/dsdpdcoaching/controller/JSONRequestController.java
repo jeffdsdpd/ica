e495ce92062095cd1cb10dc20609877675e94a8a
Jeff Kish <jeff@dsdpdcoaching.com>
2022-02-05 07:12:34 -0500

    additional ninjareport work and refactoring

--- dsdpdcoaching/src/main/java/com/dsd/dsdpdcoaching/controller/JSONRequestController.java
+++ dsdpdcoaching/src/main/java/com/dsd/dsdpdcoaching/controller/JSONRequestController.java
@@ -27,7 +27,6 @@ import com.dsd.dsdpdcoaching.dao.UserDao;
 import com.dsd.dsdpdcoaching.dto.ActionPlanData;
 import com.dsd.dsdpdcoaching.dto.CoachingData;
 import com.dsd.dsdpdcoaching.dto.NinjaLevelTeachingData;
-import com.dsd.dsdpdcoaching.dto.NinjaLevelUpStrategy;
 import com.dsd.dsdpdcoaching.dto.NinjaReportData;
 import com.dsd.dsdpdcoaching.dto.PhaseValues;
 import com.dsd.dsdpdcoaching.dto.Rubric;
@@ -38,6 +37,7 @@ import com.dsd.dsdpdcoaching.dto.TeacherProgressionReportData;
 import com.dsd.dsdpdcoaching.dto.User;
 import com.dsd.dsdpdcoaching.service.ClassroomQuickstart;
 import com.dsd.dsdpdcoaching.service.EmailService;
+import com.dsd.dsdpdcoaching.service.NinjaValueAssignments;
 import com.google.api.services.classroom.model.Course;
 import com.google.api.services.classroom.model.CourseWork;
 
@@ -64,7 +64,12 @@ public class JSONRequestController extends HttpServlet {
 	private NinjaLevelTrainingDao ninjaLevelTrainingDao;
 	@Autowired
 	private ClassroomQuickstart classroomQuickstart;
-
+	@Autowired
+	private NinjaReportData nlrd;
+	@Autowired
+	private NinjaValueAssignments nva;
+	
+	
 	//Called from coachingForm.js, coachingReport.js,rubricForm.js, and rubricReport.js
 	@GetMapping(value="/getTeachersBySchool")
 	@ResponseBody
@@ -112,220 +117,12 @@ public class JSONRequestController extends HttpServlet {
 	@GetMapping(value="/getNinjaTrainingRecordById")
 	@ResponseBody
 	public NinjaReportData getNinjaTrainingRecordById(@RequestParam Integer recordId) {
-		NinjaReportData nlrd = new NinjaReportData();
-		NinjaLevelUpStrategy nlus = new NinjaLevelUpStrategy();
-		
 		NinjaLevelTeachingData ninjaData =  ninjaLevelTrainingDao.getNinjaTrainingRecordById(recordId);
-		
 		nlrd.setUserId(ninjaData.getUserId());
-			
-		//SMALLGROUP
-		if (ninjaData.getSmallgroupwhite() != null && ninjaData.getSmallgroupwhite().equals("yes")) {
-			if (ninjaData.getSmallgroupyellow() != null && ninjaData.getSmallgroupyellow().equals("yes")) {
-				if (ninjaData.getSmallgrouporange() != null && ninjaData.getSmallgrouporange().equals("yes")) {
-					if (ninjaData.getSmallgroupgreen() != null && ninjaData.getSmallgroupgreen().equals("yes")) {
-						if (ninjaData.getSmallgroupblue() != null && ninjaData.getSmallgroupblue().equals("yes")) {
-							if (ninjaData.getSmallgrouppurple() != null && ninjaData.getSmallgrouppurple().equals("yes")) {
-								nlrd.setSmallGroupLevelUp("Complete");
-								nlrd.setSmallGroupColor("purple");
-							} else {
-								nlrd.setSmallGroupLevelUp(nlus.smallGroupPurple);
-								nlrd.setSmallGroupColor("blue");
-							}
-						} else {
-							nlrd.setSmallGroupLevelUp(nlus.smallGroupBlue);
-							nlrd.setSmallGroupColor("green");
-						}
-					} else {
-						nlrd.setSmallGroupLevelUp(nlus.smallGroupGreen);
-						nlrd.setSmallGroupColor("orange");
-					}
-				} else {
-					nlrd.setSmallGroupLevelUp(nlus.smallGroupOrange);
-					nlrd.setSmallGroupColor("yellow");
-				}
-			} else {
-				nlrd.setSmallGroupLevelUp(nlus.smallGroupYellow);
-				nlrd.setSmallGroupColor("white");
-			}
-		} else {
-			nlrd.setSmallGroupLevelUp(nlus.smallGroupWhite);
-			nlrd.setSmallGroupColor("NA");
-		}
 		
-		//CHECKLIST
-		if (ninjaData.getChecklistwhite() != null && ninjaData.getChecklistwhite().equals("yes")) {
-			if (ninjaData.getChecklistyellow() != null && ninjaData.getChecklistyellow().equals("yes")) {
-				if (ninjaData.getChecklistorange() != null && ninjaData.getChecklistorange().equals("yes")) {
-					if (ninjaData.getChecklistgreen() != null && ninjaData.getChecklistgreen().equals("yes")) {
-						if (ninjaData.getChecklistblue() != null && ninjaData.getChecklistblue().equals("yes")) {
-							if (ninjaData.getChecklistpurple() != null && ninjaData.getChecklistpurple().equals("yes")) {
-								nlrd.setChecklistLevelUp("Complete");
-								nlrd.setChecklistColor("purple");
-							} else {
-								nlrd.setChecklistLevelUp(nlus.checklistPurple);
-								nlrd.setChecklistColor("blue");
-							}
-						} else {
-							nlrd.setChecklistLevelUp(nlus.checklistBlue);
-							nlrd.setChecklistColor("green");
-						}
-					} else {
-						nlrd.setChecklistLevelUp(nlus.checklistGreen);
-						nlrd.setChecklistColor("orange");
-					}
-				} else {
-					nlrd.setChecklistLevelUp(nlus.checklistOrange);
-					nlrd.setChecklistColor("yellow");
-				}
-			} else {
-				nlrd.setChecklistLevelUp(nlus.checklistYellow);
-				nlrd.setChecklistColor("white");
-			}
-		} else {
-			nlrd.setChecklistLevelUp(nlus.checklistWhite);
-			nlrd.setChecklistColor("NA");
-		}
-		
-		//DATA
-		if (ninjaData.getDatawhite() != null && ninjaData.getDatawhite().equals("yes")) {
-			if (ninjaData.getDatayellow() != null && ninjaData.getDatayellow().equals("yes")) {
-				if (ninjaData.getDataorange() != null && ninjaData.getDataorange().equals("yes")) {
-					if (ninjaData.getDatagreen() != null && ninjaData.getDatagreen().equals("yes")) {
-						if (ninjaData.getDatablue() != null && ninjaData.getDatablue().equals("yes")) {
-							if (ninjaData.getDatapurple() != null && ninjaData.getDatapurple().equals("yes")) {
-								nlrd.setDataLevelUp("Complete");
-								nlrd.setDataColor("purple");
-							} else {
-								nlrd.setDataLevelUp(nlus.dataPurple);
-								nlrd.setDataColor("blue");
-							}
-						} else {
-							nlrd.setDataLevelUp(nlus.dataBlue);
-							nlrd.setDataColor("green");
-						}
-					} else {
-						nlrd.setDataLevelUp(nlus.dataGreen);
-						nlrd.setDataColor("orange");
-					}
-				} else {
-					nlrd.setDataLevelUp(nlus.dataOrange);
-					nlrd.setDataColor("yellow");
-				}
-			} else {
-				nlrd.setDataLevelUp(nlus.dataYellow);
-				nlrd.setDataColor("white");
-			}
-		} else {
-			nlrd.setDataLevelUp(nlus.dataWhite);
-			nlrd.setDataColor("NA");
-		}
-		
-		//STUDENT CHOICE
-		if (ninjaData.getStudentchoicewhite() != null && ninjaData.getStudentchoicewhite().equals("yes")) {
-			if (ninjaData.getStudentchoiceyellow() != null && ninjaData.getStudentchoiceyellow().equals("yes")) {
-				if (ninjaData.getStudentchoiceorange() != null && ninjaData.getStudentchoiceorange().equals("yes")) {
-					if (ninjaData.getStudentchoicegreen() != null && ninjaData.getStudentchoicegreen().equals("yes")) {
-						if (ninjaData.getStudentchoiceblue() != null && ninjaData.getStudentchoiceblue().equals("yes")) {
-							if (ninjaData.getStudentchoicepurple() != null && ninjaData.getStudentchoicepurple().equals("yes")) {
-								nlrd.setDataLevelUp("Complete");
-								nlrd.setStudentChoiceColor("purple");
-							} else {
-								nlrd.setStudentChoiceLevelUp(nlus.studentChoicePurple);
-								nlrd.setStudentChoiceColor("blue");
-							}
-						} else {
-							nlrd.setStudentChoiceLevelUp(nlus.studentChoiceBlue);
-							nlrd.setStudentChoiceColor("green");
-						}
-					} else {
-						nlrd.setStudentChoiceLevelUp(nlus.studentChoiceGreen);
-						nlrd.setStudentChoiceColor("orange");
-					}
-				} else {
-					nlrd.setStudentChoiceLevelUp(nlus.studentChoiceOrange);
-					nlrd.setStudentChoiceColor("yellow");
-				}
-			} else {
-				nlrd.setStudentChoiceLevelUp(nlus.studentChoiceYellow);
-				nlrd.setStudentChoiceColor("white");
-			}
-		} else {
-			nlrd.setStudentChoiceLevelUp(nlus.studentChoiceWhite);
-			nlrd.setStudentChoiceColor("NA");
-		}
-		
-		//INDEPENDENT STUDIO
-		if (ninjaData.getIndependentstudiowhite() != null && ninjaData.getIndependentstudiowhite().equals("yes")) {
-			if (ninjaData.getIndependentstudioyellow() != null && ninjaData.getIndependentstudioyellow().equals("yes")) {
-				if (ninjaData.getIndependentstudioorange() != null && ninjaData.getIndependentstudioorange().equals("yes")) {
-					if (ninjaData.getIndependentstudiogreen() != null && ninjaData.getIndependentstudiogreen().equals("yes")) {
-						if (ninjaData.getIndependentstudioblue() != null && ninjaData.getIndependentstudioblue().equals("yes")) {
-							if (ninjaData.getIndependentstudiopurple() != null && ninjaData.getIndependentstudiopurple().equals("yes")) {
-								nlrd.setIndependentStudioLevelUp("Complete");
-								nlrd.setIndependentStudioColor("purple");
-							} else {
-								nlrd.setIndependentStudioLevelUp(nlus.independentStudioPurple);
-								nlrd.setIndependentStudioColor("blue");
-							}
-						} else {
-							nlrd.setIndependentStudioLevelUp(nlus.independentStudioBlue);
-							nlrd.setIndependentStudioColor("green");
-						}
-					} else {
-						nlrd.setIndependentStudioLevelUp(nlus.independentStudioGreen);
-						nlrd.setIndependentStudioColor("orange");
-					}
-				} else {
-					nlrd.setIndependentStudioLevelUp(nlus.independentStudioOrange);
-					nlrd.setIndependentStudioColor("yellow");
-				}
-			} else {
-				nlrd.setIndependentStudioLevelUp(nlus.independentStudioYellow);
-				nlrd.setIndependentStudioColor("white");
-			}
-		} else {
-			nlrd.setIndependentStudioLevelUp(nlus.independentStudioWhite);
-			nlrd.setIndependentStudioColor("NA");
-		}
-		
-		//DIGITAL CONTENT
-		if (ninjaData.getDigitalcontentwhite() != null && ninjaData.getDigitalcontentwhite().equals("yes")) {
-			if (ninjaData.getDigitalcontentyellow() != null && ninjaData.getDigitalcontentyellow().equals("yes")) {
-				if (ninjaData.getDigitalcontentorange() != null && ninjaData.getDigitalcontentorange().equals("yes")) {
-					if (ninjaData.getDigitalcontentgreen() != null && ninjaData.getDigitalcontentgreen().equals("yes")) {
-						if (ninjaData.getDigitalcontentblue() != null && ninjaData.getDigitalcontentblue().equals("yes")) {
-							if (ninjaData.getDigitalcontentpurple() != null && ninjaData.getDigitalcontentpurple().equals("yes")) {
-								nlrd.setDigitalContentLevelUp("Complete");
-								nlrd.setDigitalContentColor("purple");
-							} else {
-								nlrd.setDigitalContentLevelUp(nlus.digitalContentPurple);
-								nlrd.setDigitalContentColor("blue");
-							}
-						} else {
-							nlrd.setDigitalContentLevelUp(nlus.digitalContentBlue);
-							nlrd.setDigitalContentColor("green");
-						}
-					} else {
-						nlrd.setDigitalContentLevelUp(nlus.digitalContentGreen);
-						nlrd.setDigitalContentColor("orange");
-					}
-				} else {
-					nlrd.setDigitalContentLevelUp(nlus.digitalContentOrange);
-					nlrd.setDigitalContentColor("yellow");
-				}
-			} else {
-				nlrd.setDigitalContentLevelUp(nlus.digitalContentYellow);
-				nlrd.setDigitalContentColor("white");
-			}
-		} else {
-			nlrd.setDigitalContentLevelUp(nlus.digitalContentWhite);
-			nlrd.setDigitalContentColor("NA");
-		}
-
-		return nlrd;
+		return nva.createNinjaValueAssignments(ninjaData, nlrd);
 	}
-	
+		
 	//Called from rubricReport.js
 	@GetMapping(value="/getRubricById")
 	@ResponseBody
--- dsdpdcoaching/src/main/java/com/dsd/dsdpdcoaching/dto/NinjaLevelUpStrategy.java
+++ dsdpdcoaching/src/main/java/com/dsd/dsdpdcoaching/dto/NinjaLevelUpStrategy.java
@@ -1,5 +1,8 @@
 package com.dsd.dsdpdcoaching.dto;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaLevelUpStrategy {
 	
 	public String smallGroupWhite = "Teaching the content to a few students while the other students are working on other learning studios";
@@ -23,64 +26,64 @@ public class NinjaLevelUpStrategy {
 	public String digitalContentBlue = "Remember the 55% 35% and 10% guide";
 	public String digitalContentPurple = "Provide choice for online applications";
 	
-	public String futureReadyWhite = "";
-	public String futureReadyYellow = "";
-	public String futureReadyOrange = "";
-	public String futureReadyGreen = "";
-	public String futureReadyBlue = "";
-	public String futureReadyPurple = "";
+	public String futureReadyWhite = "Students work together to complete the same activity during the studio time. Ex. gallery walks, matching, content related games, scavenger hunts";
+	public String futureReadyYellow = "Students are provided a choice board in creativity activities";
+	public String futureReadyOrange = "Students have multiple creativity choices from online programs";
+	public String futureReadyGreen = "Differentiated choice boards are provided to the students";
+	public String futureReadyBlue = "Students can create their own choice activities";
+	public String futureReadyPurple = "Students get the chance to complete project based learning activities ";
 	
-	public String placeWhite = "";
-	public String placeYellow = "";
-	public String placeOrange = "";
-	public String placeGreen = "";
-	public String placeBlue = "";
-	public String placePurple = "";
+	public String placeWhite = "Students move from one studio location to the next during studio learning dates";
+	public String placeYellow = "Visit or explore Blended Learning classroom setups to gain ideas for the classroom";
+	public String placeOrange = "Add in flexible seating within the studio areas";
+	public String placeGreen = "Redesign the learning space every month to continue to spice up the learning options";
+	public String placeBlue = "Students have the ability to work in an area that fits their own learning style but the still have to move to the small group and future ready studio area";
+	public String placePurple = "The students have the choice of working online or offline to learn the concepts or skills";
 	
-	public String pathWhite = "";
-	public String pathYellow = "";
-	public String pathOrange = "";
-	public String pathGreen = "";
-	public String pathBlue = "";
-	public String pathPurple = "";
+	public String pathWhite = "Design a one day checklist that the students complete within one class period that contains all four learning studios";
+	public String pathYellow = "Set up checklist folders for the students";
+	public String pathOrange = "Generate a two day checklist that showcases multiple learning studios";
+	public String pathGreen = "Introduce differentiated checklists";
+	public String pathBlue = "Spark the ability for students to generate their own checklist";
+	public String pathPurple = "Tie in project based learning options into the checklist";
 	
-	public String paceWhite = "";
-	public String paceYellow = "";
-	public String paceOrange = "";
-	public String paceGreen = "";
-	public String paceBlue = "";
-	public String pacePurple = "";
+	public String paceWhite = "Set a visual timer for all of the students to follow as they move through a one day checklist";
+	public String paceYellow = "Share out with the students the WHY of the timer";
+	public String paceOrange = "Implement the progression board for the students to use to keep track of then number of studios completed in one day or multiple days";
+	public String paceGreen = "Showcase different online and physical timers that the students can use during studio days";
+	public String paceBlue = "Post suggested time amounts on the checklist";
+	public String pacePurple = "Allow the students to move through the checklist at their own pace (They write out the amount of time allotment on their checklist)";
 	
-	public String dataWhite = "";
-	public String dataYellow = "";
-	public String dataOrange = "";
-	public String dataGreen = "";
-	public String dataBlue = "";
-	public String dataPurple = "";
+	public String dataWhite = "Implement exit tickets, formative assessment questions, and digital content applications that provide data points";
+	public String dataYellow = "Design a system to keep track of all of the data. Ex. Teacher Data Trackers";
+	public String dataOrange = "During the small group instruction, use a teacher tracker to design differentiated groupings";
+	public String dataGreen = "Turn the checklist folders into data folders with the deployment of student data meetings";
+	public String dataBlue = "Allow the students to explore their personal data to generate their individiual checklist";
+	public String dataPurple = "Personalize projects based on formative and summative data";
 	
-	public String directionsWhite = "";
-	public String directionsYellow = "";
-	public String directionsOrange = "";
-	public String directionsGreen = "";
-	public String directionsBlue = "";
-	public String directionsPurple = "";
-	public String directionsBrown = "";
-	public String directionsRed = "";
+	public String directionsWhite = "Post the directions in various locations including online, printed, and on checklists";
+	public String directionsYellow = "Use color to highlist key elements";
+	public String directionsOrange = "Deploy student experts";
+	public String directionsGreen = "QR Code voice directions";
+	public String directionsBlue = "Video directions - think of directions in a TikTok video fashion";
+	public String directionsPurple = "Try voice recoding devices - 'Rainbow Recorders'";
+	public String directionsBrown = "Toss in element of surprises with the directions";
+	public String directionsRed = "Emoji Code Directions";
 	
-	public String studentChoiceWhite = "";
-	public String studentChoiceYellow = "";
-	public String studentChoiceOrange = "";
-	public String studentChoiceGreen = "";
-	public String studentChoiceBlue = "";
-	public String studentChoicePurple = "";
+	public String studentChoiceWhite = "Design future ready activities that promote creativity, collaboration, and communication";
+	public String studentChoiceYellow = "Provide a choice board of future ready activities that the students can pick from that tie to the content or skill";
+	public String studentChoiceOrange = "Differentiate the choice board based on the individual skill and ability";
+	public String studentChoiceGreen = "Explore new online and offline student showcase project ideas";
+	public String studentChoiceBlue = "Add in additional choice in the other learning studios";
+	public String studentChoicePurple = "Generate lengthy choice activities that tie into a project based learning opportunity";
 	
-	public String organizationWhite = "";
-	public String organizationYellow = "";
-	public String organizationOrange = "";
-	public String organizationGreen = "";
-	public String organizationBlue = "";
-	public String organizationPurple = "";
-	public String organizationBrown = "";
+	public String organizationWhite = "Implementation of checklist";
+	public String organizationYellow = "Checklist folders or student interactive notebooks where students can store their checklist and studio materials";
+	public String organizationOrange = "Material organization system for multiple day checklist";
+	public String organizationGreen = "Effectively using individual timers";
+	public String organizationBlue = "Deployment of the teacher tracker";
+	public String organizationPurple = "System for calling students back to the differentiated small groups";
+	public String organizationBrown = "Data Cards and Data Folder";
 	
 	public String checklistWhite = "One day checklist with four studios, checkoff boxes, directions, date, and completed in one day";
 	public String checklistYellow = "Two day checklist with eight studios that are completed over two days";
